407e3ff67f6c53a71efa598ca49d1c0e4d0df954
Nelson V <nvelez@intresco.co>
2025-01-14 13:44:32 -0500

    Migración Odoo15

--- models/done.py
+++ models/done.py
@@ -1,6 +1,7 @@
 from odoo import api, fields, models,_
 from odoo.exceptions import except_orm, ValidationError ,UserError
 from collections import defaultdict
+import re
 
 
 class done_activity(models.Model):
@@ -17,98 +18,24 @@ class done_activity(models.Model):
 class mail_activity2(models.Model):
     _inherit = "mail.activity"
 
-    '''def action_feedback(self, feedback=False):
-        message = self.env['mail.message']
-        obj = self.env['done.activity']
-        if feedback:
-            self.write(dict(feedback=feedback))
-            
-        for activity in self:
-            record = self.env[activity.res_model].browse(activity.res_id)
-            record.message_post_with_view(
-                'mail.message_activity_done',
-                values={'activity': activity},
-                subtype_id=self.env['ir.model.data'].xmlid_to_res_id('mail.mt_activities'),
-                mail_activity_type_id=activity.activity_type_id.id,
-            )
-            obj.create({'name': activity.summary,'type':activity.activity_type_id.name,'state':'done','user':activity.user_id.name})
-            message |= record.message_ids[0] 
-
-        self.unlink()
-        return message.ids and message.ids[0] or False'''
-
     def _action_done(self, feedback=False, attachment_ids=None):
-        """ Private implementation of marking activity as done: posting a message, deleting activity
-            (since done), and eventually create the automatical next activity (depending on config).
-            :param feedback: optional feedback from user when marking activity as done
-            :param attachment_ids: list of ir.attachment ids to attach to the posted mail.message
-            :returns (messages, activities) where
-                - messages is a recordset of posted mail.message
-                - activities is a recordset of mail.activity of forced automically created activities
-        """
-        # marking as 'done'
-        messages = self.env['mail.message']
+        messages, next_activities = super(mail_activity2, self)._action_done(feedback=feedback, attachment_ids=attachment_ids)
         obj = self.env['done.activity']
-        next_activities_values = []
+        for message in messages:
+            activity = message.description
+            name = activity.split('\n', 1)[0]
+            name = name.replace(' hecho', '')
+            match = re.search(r'\n:(.*?)\n', activity)
+            if match:
+                description = match.group(1).strip()
+            else:
+                description = ''
 
-        # Search for all attachments linked to the activities we are about to unlink. This way, we
-        # can link them to the message posted and prevent their deletion.
-        attachments = self.env['ir.attachment'].search_read([
-            ('res_model', '=', self._name),
-            ('res_id', 'in', self.ids),
-        ], ['id', 'res_id'])
-
-        activity_attachments = defaultdict(list)
-        for attachment in attachments:
-            activity_id = attachment['res_id']
-            activity_attachments[activity_id].append(attachment['id'])
-
-        for activity in self:
-            # extract value to generate next activities
-            if activity.force_next:
-                Activity = self.env['mail.activity'].with_context(activity_previous_deadline=activity.date_deadline)  # context key is required in the onchange to set deadline
-                vals = Activity.default_get(Activity.fields_get())
-
-                vals.update({
-                    'previous_activity_type_id': activity.activity_type_id.id,
-                    'res_id': activity.res_id,
-                    'res_model': activity.res_model,
-                    'res_model_id': self.env['ir.model']._get(activity.res_model).id,
-                })
-                virtual_activity = Activity.new(vals)
-                virtual_activity._onchange_previous_activity_type_id()
-                virtual_activity._onchange_activity_type_id()
-                next_activities_values.append(virtual_activity._convert_to_write(virtual_activity._cache))
-
-            # post message on activity, before deleting it
-            record = self.env[activity.res_model].browse(activity.res_id)
-            record.message_post_with_view(
-                'mail.message_activity_done',
-                values={
-                    'activity': activity,
-                    'feedback': feedback,
-                    'display_assignee': activity.user_id != self.env.user
-                },
-                subtype_id=self.env['ir.model.data'].xmlid_to_res_id('mail.mt_activities'),
-                mail_activity_type_id=activity.activity_type_id.id,
-                attachment_ids=[(4, attachment_id) for attachment_id in attachment_ids] if attachment_ids else [],
-            )
-
-            # Moving the attachments in the message
-            # TODO: Fix void res_id on attachment when you create an activity with an image
-            # directly, see route /web_editor/attachment/add
-            obj.create({'name': activity.summary,'type':activity.activity_type_id.name,'state':'Finalizado','user':activity.user_id.name,'contact':activity.res_name,'date_mod':activity.write_date})
-            activity_message = record.message_ids[0]
-            message_attachments = self.env['ir.attachment'].browse(activity_attachments[activity.id])
-            if message_attachments:
-                message_attachments.write({
-                    'res_id': activity_message.id,
-                    'res_model': activity_message._name,
-                })
-                activity_message.attachment_ids = message_attachments
-            messages |= activity_message
-
-        next_activities = self.env['mail.activity'].create(next_activities_values)
-        self.unlink()  # will unlink activity, dont access `self` after that
-
+            obj.create({
+                'name': description,
+                'contact': message.display_name,
+                'type':name,
+                'state':'Finalizado',
+                'user':message.author_id.name,
+            })
         return messages, next_activities
